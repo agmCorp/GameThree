d6734999f9ce15c05f06d7d4e199c10e7e5cb1d9
Alvaro G. Morales <alvaro.gabriel.morales@gmail.com>
2018-01-10 00:30:41 -0300

    No comments

--- core/src/uy/com/agm/gamethree/game/Constants.java
+++ core/src/uy/com/agm/gamethree/game/Constants.java
@@ -30,7 +30,7 @@ public class Constants {
     public static final float GAMECAM_VELOCITY = 1.0f;
 
     // Debug mode enabled by default
-    public static final boolean DEBUG_MODE = true;
+    public static final boolean DEBUG_MODE = false;
 
     // ---- Assets ----
 
@@ -123,7 +123,7 @@ public class Constants {
     public static final float POWERONE_FADING_SECONDS = 5.0f;
     public static final float POWERONE_WIDTH_METERS = 52.0f * 1.0f / PPM;
     public static final float POWERONE_HEIGHT_METERS = 52.0f * 1.0f / PPM;
-    public static final int TIMER_POWERONE = 200;
+    public static final int TIMER_POWERONE = 20;
     public static final int POWERONE_SCORE = 15;
     public static final float POWERONE_FX_WIDTH_METERS = 139.0f * 1.4f / PPM;
     public static final float POWERONE_FX_HEIGHT_METERS = 137.0f * 1.4f / PPM;
@@ -165,7 +165,7 @@ public class Constants {
     public static final float POWERFOUR_FADING_SECONDS = 5.0f;
     public static final float POWERFOUR_WIDTH_METERS = 192.0f * 0.5f / PPM;
     public static final float POWERFOUR_HEIGHT_METERS = 192.0f * 0.5f / PPM;
-    public static final int TIMER_POWERFOUR = 400;
+    public static final int TIMER_POWERFOUR = 20;
     public static final int POWERFOUR_SCORE = 30;
     public static final float POWERFOUR_FX_WIDTH_METERS = 139.0f * 1.4f / PPM;
     public static final float POWERFOUR_FX_HEIGHT_METERS = 137.0f * 1.4f / PPM;
