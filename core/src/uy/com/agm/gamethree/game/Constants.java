68c92eb5035ee7961d4fb0e75ae410d6e110e5e1
Alvaro G. Morales <alvaro.gabriel.morales@gmail.com>
2018-01-22 02:27:46 -0300

    No comments

--- core/src/uy/com/agm/gamethree/game/Constants.java
+++ core/src/uy/com/agm/gamethree/game/Constants.java
@@ -32,7 +32,7 @@ public class Constants {
     public static final float GAMECAM_VELOCITY = 1.0f;
 
     // Debug mode enabled by default
-    public static final boolean DEBUG_MODE = true;
+    public static final boolean DEBUG_MODE = false;
 
     public static final String SETTINGS = "uy.com.agm.gameThree.settings";
 
