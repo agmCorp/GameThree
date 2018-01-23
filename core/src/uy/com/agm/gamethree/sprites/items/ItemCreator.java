5ff159b8412fa2c540218152b1d037daa31f2f81
amorales <alvaro.gabriel.morales@gmail.com>
2018-01-23 17:44:53 -0300

    No comments

--- core/src/uy/com/agm/gamethree/sprites/items/ItemCreator.java
+++ core/src/uy/com/agm/gamethree/sprites/items/ItemCreator.java
@@ -3,6 +3,7 @@ package uy.com.agm.gamethree.sprites.items;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapProperties;
 
+import uy.com.agm.gamethree.sprites.items.collectibles.ColOne;
 import uy.com.agm.gamethree.sprites.items.powerups.PowerFour;
 import uy.com.agm.gamethree.sprites.items.powerups.PowerOne;
 import uy.com.agm.gamethree.sprites.items.powerups.PowerThree;
@@ -18,7 +19,7 @@ public class ItemCreator {
     public static void getItemOnHit(MapObject object, B2WorldCreator creator, float x, float y) {
         MapProperties mp = object.getProperties();
         if (mp.containsKey("colOne")) {
-            creator.createGameThreeActor(new GameThreeActorDef(x, y, PowerOne.class));
+            creator.createGameThreeActor(new GameThreeActorDef(x, y, ColOne.class));
         }
         if (mp.containsKey("powerOne")) {
             creator.createGameThreeActor(new GameThreeActorDef(x, y, PowerOne.class));
