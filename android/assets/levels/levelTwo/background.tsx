bcc75a2a5b89a4af87117cb8cd9ccb9d3b059bad
Alvaro G. Morales <alvaro.gabriel.morales@gmail.com>
2018-04-17 00:32:38 -0300

    No comments

--- android/assets/levels/levelTwo/background.tsx
+++ android/assets/levels/levelTwo/background.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="background" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
- <image source="../background.png" trans="ffffff" width="2048" height="2048"/>
+ <image source="../backgroundOneTwo.png" trans="ffffff" width="2048" height="2048"/>
 </tileset>
