2fbdcd912a4805b2e8657eefdfcdc27f5636b247
amorales <alvaro.gabriel.morales@gmail.com>
2018-04-24 19:05:13 -0300

    No comments

--- core/src/uy/com/agm/gamethree/assets/sprites/AssetColOne.java
+++ core/src/uy/com/agm/gamethree/assets/sprites/AssetColOne.java
@@ -15,7 +15,7 @@ public class AssetColOne {
     private static final String TAG = AssetColOne.class.getName();
 
     // Constants (meters = pixels * resizeFactor / PPM)
-    public static final float WIDTH_METERS = 84.0f * 0.5f / PlayScreen.PPM;
+    public static final float WIDTH_METERS = 85.0f * 0.5f / PlayScreen.PPM;
     public static final float HEIGHT_METERS = 85.0f * 0.5f / PlayScreen.PPM;
 
     private TextureRegion goldStand;
@@ -34,17 +34,17 @@ public class AssetColOne {
 
         // Animation
         regions = atlas.findRegions("gold");
-        goldAnimation = new Animation(0.5f / 10.0f, regions, Animation.PlayMode.LOOP);
+        goldAnimation = new Animation(0.3f / 10.0f, regions, Animation.PlayMode.LOOP);
         regions.clear();
 
         // Animation
         regions = atlas.findRegions("silver");
-        silverAnimation = new Animation(0.5f / 10.0f, regions, Animation.PlayMode.LOOP);
+        silverAnimation = new Animation(0.3f / 10.0f, regions, Animation.PlayMode.LOOP);
         regions.clear();
 
         // Animation
         regions = atlas.findRegions("bronze");
-        bronzeAnimation = new Animation(0.5f / 10.0f, regions, Animation.PlayMode.LOOP);
+        bronzeAnimation = new Animation(0.3f / 10.0f, regions, Animation.PlayMode.LOOP);
         regions.clear();
     }
 
