3420395334da8208fd432346a466025e02fa17fb
amorales <alvaro.gabriel.morales@gmail.com>
2018-04-25 15:33:06 -0300

    No comments

--- core/src/uy/com/agm/gamethree/screens/PlayScreen.java
+++ core/src/uy/com/agm/gamethree/screens/PlayScreen.java
@@ -152,14 +152,14 @@ public class PlayScreen extends AbstractScreen {
             b2dr = new Box2DDebugRenderer();
         }
 
-        final float ALTURA = 70.0f; // todo
-        gameCam.position.set(gameViewPort.getWorldWidth() / 2, ALTURA, 0); // todo
+        //final float ALTURA = 70.0f; // todo
+        //gameCam.position.set(gameViewPort.getWorldWidth() / 2, ALTURA, 0); // todo
 
         creator = new B2WorldCreator(this);
 
         // Create the hero in our game world
         player = new Hero(this, gameCam.position.x, gameCam.position.y / 2);
-        player.getB2body().setTransform(this.getGameCam().position.x, this.getGameCam().position.y - this.getGameViewPort().getWorldHeight() / 4, player.getB2body().getAngle()); //todo
+        //player.getB2body().setTransform(this.getGameCam().position.x, this.getGameCam().position.y - this.getGameViewPort().getWorldHeight() / 4, player.getB2body().getAngle()); //todo
 
         // Boundaries
         upperEdge = creator.getUpperEdge();
