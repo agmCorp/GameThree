02485c6d86e617f8336b542cc31f47a7cc31086d
Alvaro G. Morales <alvaro.gabriel.morales@gmail.com>
2018-06-01 01:58:11 -0300

    No comments

--- core/src/uy/com/agm/gamethree/assets/fonts/AssetFonts.java
+++ core/src/uy/com/agm/gamethree/assets/fonts/AssetFonts.java
@@ -12,9 +12,9 @@ public class AssetFonts {
     private static final String TAG = AssetFonts.class.getName();
 
     // Constants
-    private static final float FONT_SMALL = 0.6f;
-    private static final float FONT_NORMAL = 0.9f;
-    private static final float FONT_BIG = 1.4f;
+    private static final float FONT_SMALL = 0.3f;
+    private static final float FONT_NORMAL = 0.5f;
+    private static final float FONT_BIG = 0.9f;
     private static final String FONT_FILE = "fonts/fonts.fnt";
 
     private BitmapFont defaultSmall;
