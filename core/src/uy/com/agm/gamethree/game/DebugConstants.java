60f20995ad7576cf367014f70e6676b3c29dcdb2
Alvaro G. Morales <alvaro.gabriel.morales@gmail.com>
2018-09-02 20:35:08 -0300

    No comments

--- core/src/uy/com/agm/gamethree/game/DebugConstants.java
+++ core/src/uy/com/agm/gamethree/game/DebugConstants.java
@@ -6,7 +6,7 @@ package uy.com.agm.gamethree.game;
 
 public class DebugConstants {
     // Master variable: turns debug mode on or off
-    public static final boolean TURN_ON_DEBUG = false;
+    public static final boolean TURN_ON_DEBUG = true;
 
     // Sets the log level to debug
     public static final boolean DEBUG_MODE = true && TURN_ON_DEBUG;
@@ -18,7 +18,7 @@ public class DebugConstants {
     public static final boolean HIDE_BACKGROUND = false && TURN_ON_DEBUG;
 
     // Shows/hides FPS counter
-    public static final boolean SHOW_FPS = false && TURN_ON_DEBUG;
+    public static final boolean SHOW_FPS = true && TURN_ON_DEBUG;
 
     // Prints box2d bodies count
     public static final boolean DEBUG_BODY_COUNT = false && TURN_ON_DEBUG;
